71ec56709a15157dd341b11f06a7ec344787dcb0
DONA PAUL <donapaul901@gmail.com>
2026-01-20 10:51:13 +0530

    todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Todo() {
     const [todos,setTodo] = useState([])
+    useEffect(() => {
+        fetch("https://dummyjson.com/todos").then(res => res.json()).then(data => {
+            setTodo(data.todos);
+        })
+    },[])
+
+
+
   return (
     <div>
         <h1>Todo list</h1>
         <p>Total Todos: {todos.length}</p>
+        <ul>{todos.map(todo =>(
+        <li><span>{todo.todo}</span><span>{todo.completed?"✅ Completed" : "❌ Not completed"}</span></li>
+        ))}</ul>
     </div>
   )
 }
