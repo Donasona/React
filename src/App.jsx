21852372e8940a18c86e242b231144931d41add6
DONA PAUL <donapaul901@gmail.com>
2026-01-20 09:43:41 +0530

    todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './assets/components/Navbar'
+import Todo from './components/Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Welcome React Component</h1>
-        <Navbar></Navbar>
-      </div> 
+      <Todo></Todo>
     </>
   )
 }
